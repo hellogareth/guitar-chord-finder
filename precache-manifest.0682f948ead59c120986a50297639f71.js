4be5242d4d907214ee3b87fc0066709b6261ddae
hellogareth <hello@gareth.work>
2019-03-13 17:38:54 +0700

    Updates

---
+++ precache-manifest.0682f948ead59c120986a50297639f71.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5a62dec41adf87f897f6",
+    "url": "/guitar-chord-finder/static/css/main.7e8cb5f5.chunk.css"
+  },
+  {
+    "revision": "5a62dec41adf87f897f6",
+    "url": "/guitar-chord-finder/static/js/main.5a62dec4.chunk.js"
+  },
+  {
+    "revision": "3536198c12b753f101bc",
+    "url": "/guitar-chord-finder/static/js/1.3536198c.chunk.js"
+  },
+  {
+    "revision": "a68c5ff911080690a669",
+    "url": "/guitar-chord-finder/static/js/runtime~main.a68c5ff9.js"
+  },
+  {
+    "revision": "c33c21d462cdd0d70a19362b98e71145",
+    "url": "/guitar-chord-finder/index.html"
+  }
+];
